84bfd28ffce52398b20b63f794658d2efa7f7c17
ubuntu <ubuntu@box157.localdomain>
2016-09-30 18:25:34 +0000

    Version 0.1.18
    e3aef3d83744ee2547385bc5619fc79fbe4bc49c

--- SuggestGrid.PCL/Models/Metadata.cs
+++ SuggestGrid.PCL/Models/Metadata.cs
@@ -16,10 +16,8 @@ using SuggestGrid;
 
 namespace SuggestGrid.Models
 {
-    public class Metadata<TKey, TValue> : Dictionary<string, object>, INotifyPropertyChanged
+    public class Metadata : INotifyPropertyChanged 
     {
-        public Metadata() : base(){}
-        public Metadata(int capacity) : base(capacity){}
         // These fields hold the values for the public properties.
         private string id;
 
@@ -28,16 +26,15 @@ namespace SuggestGrid.Models
         /// Id parameter is necessary for all metadata.
         /// </summary>
         [JsonProperty("id")]
-        public string Id
-        {
-            get
+        public string Id 
+        { 
+            get 
             {
-                return this.id;
-            }
-            set
+                return this.id; 
+            } 
+            set 
             {
                 this.id = value;
-                this.Add ("id", value);
                 onPropertyChanged("Id");
             }
         }
@@ -59,4 +56,4 @@ namespace SuggestGrid.Models
             }
         }
     }
-}
+} 
