13f85407404207cdd452f38e0296050ce1c00453
ubuntu <ubuntu@box573.localdomain>
2017-01-18 01:15:58 +0000

    Fix more-like-this query generation
    37aa49c3a31a8dae87cb05a954b95a0680083abf

--- SuggestGrid/Controllers/BaseController.cs
+++ SuggestGrid/Controllers/BaseController.cs
@@ -1,7 +1,7 @@
 /*
  * SuggestGrid.PCL
  *
- * This file was automatically generated for SuggestGrid by APIMATIC v2.0 ( https://apimatic.io ) on 12/18/2016
+ * This file was automatically generated for SuggestGrid by APIMATIC v2.0 ( https://apimatic.io ) on 01/18/2017
  */
 using System;
 using SuggestGrid;
